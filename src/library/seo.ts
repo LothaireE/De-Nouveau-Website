08c32d7d82b68ed66214321c7b000136d7d04b21
LothaireE <lothaire.epee@gmail.com>
2026-06-12 11:32:03 +0200

    google search result Icon

--- src/library/seo.ts
+++ src/library/seo.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-const siteUrl = "https://www.de-nouveau.fr";
+const siteUrl = "https://www.denouveau.fr";
 
 type SeoParams = {
     title: string;
@@ -15,21 +15,20 @@ export function createMetadata({
     description,
     path,
     locale,
-    image = "/DE_NOUVEAU/PNG/og-default-de-nouveau.png", // "/images/og-default.jpg",
+    image = "/DE_NOUVEAU/PNG/og-default-de-nouveau.png", // Open Graph image for thumbnails
 }: SeoParams): Metadata {
     // only a French version of the site, so hardcoded if we add English content, we should uncomment to determine the language based on the locale
     // const isFrench = locale === "fr_FR";
     const url = `${siteUrl}${path}`;
     const imageUrl = image.startsWith("http") ? image : `${siteUrl}${image}`;
-    const favIcon = "/DE_NOUVEAU/PNG/AAAA_BLACK02.png";
 
     return {
         title,
         description,
         icons: {
-            icon: favIcon,
-            shortcut: favIcon,
-            apple: favIcon,
+            icon: "/icon.png",
+            shortcut: "/icon.png",
+            apple: "/icon.png",
         },
         metadataBase: new URL(siteUrl),
         alternates: {
