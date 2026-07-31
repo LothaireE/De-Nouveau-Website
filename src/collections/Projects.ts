ffbf6dabc53a26e2ffb3ec0c3b1447e6e5c5fc4d
LothaireE <lothaire.epee@gmail.com>
2026-07-31 11:54:44 +0200

    Restrict plans to image media

--- src/collections/Projects.ts
+++ src/collections/Projects.ts
@@ -256,6 +256,11 @@ export const Projects: CollectionConfig = {
                     name: "image",
                     type: "upload",
                     relationTo: "media",
+                    filterOptions: {
+                        mediaType: {
+                            equals: "image",
+                        },
+                    },
                 },
                 {
                     name: "layout",
